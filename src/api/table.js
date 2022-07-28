9ea64c913ab695598de13a36b60930924f58664e
RowanWang6 <sos31131@qq.com>
2022-07-28 16:18:11 +0800

    新增待审核列表、已审核记录、三种发票的具体审核页面

--- src/api/table.js
+++ src/api/table.js
@@ -1,10 +1,36 @@
 import request from '@/utils/request'
 
-// 用户提交历史申报
+// 待审核Api
 export function getList(data) {
   return request({
-    url: '/history/user',
+    url:  '/audit/list',
     method: 'post',
     data,
   })
 }
+
+// 已审核Api
+export function getHistory(data) {
+  return request({
+    url:  '/audit/history',
+    method: 'post',
+    data,
+  })
+}
+
+export function show(id) {
+  return request({
+    url: `/history/info/detail/${id}`,
+    method: 'get',
+    // params: data,
+  })
+}
+
+export function saveStatus(data) {
+  return request({
+    url:  '/audit/check',
+    method: 'post',
+    data,
+  })
+}
+
